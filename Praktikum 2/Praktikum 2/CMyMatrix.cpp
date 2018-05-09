411bc93c386fe1e22efcf2e010d0f1d515da56ec
mhenn <mikeianhenning93@gmail.com>
2018-05-09 10:58:16 +0200

    matrix geschisse

---
+++ Praktikum 2/Praktikum 2/CMyMatrix.cpp
@@ -0,0 +1,3 @@
+#include "CMyMatrix.h"
+CMyMatrix invers() {}
+
--- Praktikum 2/Praktikum 2/CMyMatrix.h
+++ Praktikum 2/Praktikum 2/CMyMatrix.h
@@ -1,17 +1,21 @@
 #pragma once
-#include <map>
-#include <vector>
+#include "CMyVektor.h"
 
 
 class CMyMatrix {
 private:
 	int dimension;
-	std::map<double, std::vector<double>> components;
+	std::vector<CMyVektor> components;
+	
 public:
 	CMyMatrix(int newDim) : dimension(newDim) {}
-	void setComponent(int x, int y, double newComp);
-	double getComponent(int x, int y, double newComp);
+	void setComponent(int x, int y, double newComp) { this->components[x][y] = newComp; }
+	double getComponent(int x, int y, double newComp) { return this->components[x][y]; }
+	void setDimension(int newDimension) { this->dimension = newDimension; }
+	int getDimension() { return this->dimension; }
 	CMyMatrix invers();
+	double operator()(int x, int y) { return getComponent(x, y); }
+	//double& operator[](int x) { return this->components[x]; }
 	
 
 
