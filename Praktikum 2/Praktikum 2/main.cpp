580a9514c5976370cf8f0fb8e05e381d9e5e9065
mhenn <mikeianhenning93@gmail.com>
2018-05-15 20:47:20 +0200

    zusatzgeschisse

--- Praktikum 2/Praktikum 2/main.cpp
+++ Praktikum 2/Praktikum 2/main.cpp
@@ -18,18 +18,19 @@ CMyVektor g(CMyVektor x) {
 	return func;
 }
 
+
+/*
 CMyVektor fZusatz(CMyVektor x) {
-	CMyVektor func(1);
-	func[0] = (sin(x(0) + pow(x(1), 2)) + pow(x(1), 3) - 6 * pow(x(1), 2) + 9 * x(1));
-	return func;
+	return (sin(x(0) + pow(x(1), 2)) + pow(x(1), 3) - 6 * pow(x(1), 2) + 9 * x(1));
 }
+*/
+CMyVektor fZusatz(CMyVektor x) {
 
-CMyVektor gZusatz(CMyVektor x) {
-	CMyVektor func(1);
-	func[0] = -(2 * pow(x(0), 2) - 2 * (x(0) * x(1)) + pow(x(1), 2) + pow(x(2), 2) - 2 * x(0) - 4 * x(2));
-	return func;
+	CMyVektor f(2);
+	f[0] = cos(x(0) + pow(x(1), 2));
+	f[1] = cos(x(0) + pow(x(1), 2)) * 2 * x(1) + 3 * pow(x(1),2) - 12* x(1) + 9;
+	return f;
 }
-
 
 
 CMyMatrix jacobi(CMyVektor x, CMyVektor(*f)(CMyVektor x)) {
@@ -92,6 +93,8 @@ void newtonVerfahren(CMyVektor x, CMyVektor(*f)(CMyVektor x)) {
 }
 
 
+
+
 void newtonExtremStelle(CMyVektor x, CMyVektor(*f)(CMyVektor x)) {
 	const double stop = 0.00001;
 
@@ -132,7 +135,7 @@ void newtonExtremStelle(CMyVektor x, CMyVektor(*f)(CMyVektor x)) {
 int main() {
 
 	CMyMatrix t(4,3);
-	CMyVektor zusatz1(2),zusatz2(3), newton(2), jacob(4);
+	CMyVektor zusatz1(2), newton(2), jacob(4);
 	zusatz1[0] = 3; zusatz1[1] = 2;
 	newton[0] = 1;	newton[1] = 1;
 	jacob[0] = 1; jacob[1] = 2; 
@@ -154,7 +157,9 @@ int main() {
 			newtonVerfahren(newton, &g);
 			break;
 		case 2:
-			newtonExtremStelle(zusatz1, &fZusatz);
+			newtonVerfahren(zusatz1, &fZusatz);
+			//zusatz_jacobi();
+			//newtonExtremStelle(zusatz1, &fZusatz);
 			break;
 		}
 	
