3ee62366b2c43bcd8720e7ce9b6d37fd4f98805f
mhenn <mikeianhenning93@gmail.com>
2018-05-29 10:51:18 +0200

    :shipit:

--- Prak 3/Prak 3/C_DGLSolver.h
+++ Prak 3/Prak 3/C_DGLSolver.h
@@ -1,8 +1,17 @@
 #pragma once
+#include "CMyVektor.h"
+
 class C_DGLSolver
 {
+private:
+	typedef CMyVektor (*f_DGL_System)(CMyVektor y, double x);
+	typedef double(*f_DGL_nterOrdnung)(CMyVektor y, double x);
+
+
 public:
 	C_DGLSolver();
 	~C_DGLSolver();
+	CMyVektor ableitungen(CMyVektor y, double x);
+
 };
 
